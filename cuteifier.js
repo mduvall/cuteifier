cdef6cd38e9426bdd70d7a4b7ac7227d7e263d8e
Matt DuVall <mduvall89@gmail.com>
2013-12-17 23:34:02 -0800

    what is this

--- cuteifier.js
+++ cuteifier.js
@@ -8,9 +8,11 @@ var escodegen = require('escodegen'),
 process.argv.forEach(function (val, index, array) {
     if (index > 1) {
         fs.readFile(val, 'utf8', function(err, data) {
-            parsedFileAST = esprima.parse(data, {comment: true, attachComment: true});
+            parsedFileAST = esprima.parse(data, {attachComment: true});
             console.log(util.inspect(parsedFileAST, false, null));
-            generatedSource = escodegen.generate(parsedFileAST, {comment: true});
+            generatedSource = escodegen.generate(parsedFileAST, {
+                comment: true
+            });
             console.log(generatedSource);
         });
     }
--- examples/comment.js
+++ examples/comment.js
@@ -1,16 +1,26 @@
 var obj =
 {
-    // Pre-a comment
     a: function()
     {
         var foo = 1; // hello
     }
-
-    /**
-     * This is some documentation for this function
-     */
     ,b: function()
     {
 
     }
 };
+
+// comment above
+function bar()
+{
+    // comment below
+    var i = 1;
+}
+
+/**
+ * This is some documentation for this function
+ */
+function foo()
+{
+
+}
